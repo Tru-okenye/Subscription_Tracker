e3756aefd4dbdd89f19d793c2a42fd0ed72d3a08
Truphy Okenye <truphenaokenye@gmail.com>
2026-03-03 13:02:24 +0300

    Fix lint errors - remove inline styles

--- App.tsx
+++ App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
   console.log('App is rendering');
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-      <Text style={{ fontSize: 30, color: 'black' }}>
+    <View style={styles.container}>
+      <Text style={styles.title}>
         I'm about to build something cool!
       </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 30,
+    color: 'black',
+  },
+});
 
 export default App;
